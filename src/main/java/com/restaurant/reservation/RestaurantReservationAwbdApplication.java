6a20e2da1041974d0e7f162f434c013ddae83f5f
Alexia Aldea <alexia.aldea@mready.net>
2025-03-20 10:10:23 +0200

    First commit

---
+++ src/main/java/com/restaurant/reservation/RestaurantReservationAwbdApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationAwbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantReservationAwbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restaurant/reservation/RestaurantReservationAwbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationAwbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
